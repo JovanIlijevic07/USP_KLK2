f954b8c4965aa58f3d54c9cc4050d0046049d856
JovanIlijevic07 <jovan.ilijevic.21@singimail.rs>
2024-10-18 09:51:59 +0200

    pull request

--- USP_Infrastructure/obj/Debug/net8.0/USP_Infrastructure.AssemblyInfo.cs
+++ USP_Infrastructure/obj/Debug/net8.0/USP_Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("USP_Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf6e5ca64af40195408389992dc071bf754c4bf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("USP_Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("USP_Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
